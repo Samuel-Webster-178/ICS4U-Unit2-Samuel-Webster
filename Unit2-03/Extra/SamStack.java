531be18d32a479b2fb21eca861bfeac312538f95
Samuel-Webster-178 <99287154+Samuel-Webster-178@users.noreply.github.com>
2024-04-10 18:24:31 -0400

    extra

---
+++ Unit2-03/Extra/SamStack.java
@@ -0,0 +1,78 @@
+/*
+* This program calculates the energy released when mass is converted to energy.
+*
+* @author  Samuel Webster
+* @version 1.0
+* @since   2024-03-25
+*/
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+* This is my stack.
+*/
+public class SamStack {
+    /** The internal variable that stores stack. */
+    private List<String> strstack = new ArrayList<String>();
+
+    /**
+    * Push a string to end of stack.
+    *
+    * @param input user input string
+    */
+    public void push(String input) {
+        strstack.add(input);
+    }
+
+    /**
+    * Checks if stack is empty.
+    *
+    * @return if stack is empty
+    */
+    public get isEmpty() {
+        return strstack.size() == 0
+    }
+
+    /**
+    * Returns stack size.
+    *
+    * @return stack length
+    */
+    public get size() {
+        return strstack.size()
+    }
+
+
+    /**
+    * Show all items on stack.
+    *
+    * @return the full stack
+    */
+    public String show() {
+        String values = "";
+        for (int counter = 0; counter < strstack.size(); counter++) {
+            values = values + strstack.get(counter) + ", ";
+        }
+        if (strstack.size() > 0) {
+            values = values.substring(0, values.length() - 2);
+        }
+        return values;
+    }
+
+    /**
+    * Pop last item from stack.
+    *
+    * @return the popped item
+    */
+    public String pop() {
+        String removed = "";
+        if (strstack.size() > 0) {
+            removed = strstack.get(strstack.size() - 1);
+            strstack.remove(strstack.size() - 1);
+        } else {
+            removed = "nothing to remove";
+        }
+        return removed;
+    }
+}
---
+++ Unit2-03/Extra/TestCases.java
@@ -0,0 +1,68 @@
+/*
+* This program calculates the energy released when mass is converted to energy.
+*
+* @author  Samuel Webster
+* @version 1.0
+* @since   2024-03-25
+*/
+
+/**
+* This runs testcases.
+*/
+final class TestCases {
+
+    /**
+    * Prevent instantiation.
+    * Throw an exception IllegalStateException.
+    * if this is ever called
+    *
+    * @throws IllegalStateException if this is ever called
+    *
+    */
+    private TestCases() {
+        throw new IllegalStateException("Cannot be instantiated");
+    }
+
+    /**
+    * The starting main() function.
+    *
+    * @param args No args will be used
+    */
+    public static void main(final String[] args) {
+        final String popString = "popped: ";
+
+        // input
+        final SamStack fruits = new SamStack();
+        fruits.push("apple");
+        fruits.push("banana");
+        System.out.println(popString + fruits.pop());
+        System.out.println(popString + fruits.pop());
+        System.out.println(popString + fruits.pop());
+        System.out.println("fruits: " + fruits.show());
+        System.out.println("length: " + fruits.length());
+        System.out.println("is empty: " + fruits.isEmpty());
+
+        final SamStack colours = new SamStack();
+        colours.push("red");
+        colours.push("green");
+        colours.push("blue");
+        System.out.println("length: " + fruits.length());
+        System.out.println(popString + colours.pop());
+        System.out.println("colours: " + colours.show());
+        System.out.println("is empty: " + fruits.isEmpty());
+
+        final SamStack animals = new SamStack();
+        animals.push("cat");
+        animals.push("dog");
+        animals.push("wolf");
+        animals.push("deer");
+        animals.push("rabbit");
+        System.out.println(popString + animals.pop());
+        System.out.println(popString + animals.pop());
+        System.out.println(popString + animals.pop());
+        System.out.println("animals: " + animals.show());
+        System.out.println("length: " + fruits.length());
+
+        System.out.println("\nDone.");
+    }
+}
