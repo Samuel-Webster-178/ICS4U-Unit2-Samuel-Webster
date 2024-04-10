98c2ad1c6aab13201c5de50678649092fbb6b8f2
Samuel-Webster-178 <99287154+Samuel-Webster-178@users.noreply.github.com>
2024-04-10 18:06:33 -0400

    aaaa

--- Unit2-06/Extra/FlyTest.java
+++ Unit2-06/Extra/FlyTest.java
@@ -12,8 +12,6 @@
 public final class FlyTest {
     /**
     * Constructor.
-    *
-    * @param args no arguments
     */
     private FlyTest() {
     }
@@ -24,22 +22,22 @@ public final class FlyTest {
     * @param args no arguments
     */
     public static void main(String[] args) {
-        Airplane biplane = new Airplane();
+        final Airplane biplane = new Airplane();
         biplane.setSpeed(212);
         System.out.println(biplane.getSpeed());
-        Jet boeing = new Jet();
+        final Jet boeing = new Jet();
         boeing.setSpeed(422);
         System.out.println(boeing.getSpeed());
-        int x = 0;
-        while(x < 4) {
+        int i = 0;
+        while (i < 4) {
             boeing.accelerate();
             System.out.println(boeing.getSpeed());
-            if(boeing.getSpeed() > 5000) {
+            if (boeing.getSpeed() > 5000) {
                 biplane.setSpeed(biplane.getSpeed() * 2);
             } else {
                 boeing.accelerate();
             }
-            x++;
+            i++;
         }
         System.out.println(biplane.getSpeed());
     }
