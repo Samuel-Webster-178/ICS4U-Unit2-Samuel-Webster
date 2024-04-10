f68141c0cde7c7d5c7e69f363db1c281c331ec7f
Samuel Webster <samuel.webster515@gmail.com>
2024-04-10 13:59:42 +0000

    init

--- Unit2-06/Extra/FlyTest.java
+++ Unit2-06/Extra/FlyTest.java
@@ -12,15 +12,15 @@ public class FlyTest {
         Airplane biplane = new Airplane();
         biplane.setSpeed(212);
         System.out.println(biplane.getSpeed());
-        Jet boeing = new Jet(422);
-        boeing.setSpeed(boeing.getSpeed());
+        Jet boeing = new Jet();
+        boeing.setSpeed(422);
         System.out.println(boeing.getSpeed());
         int x = 0;
         while(x < 4) {
             boeing.accelerate();
             System.out.println(boeing.getSpeed());
-            if(being.getSpeed() > 5000) {
-                biplane.setSpeed(biplane.getSpeed * 2)
+            if(boeing.getSpeed() > 5000) {
+                biplane.setSpeed(biplane.getSpeed() * 2);
             } else {
                 boeing.accelerate();
             }
