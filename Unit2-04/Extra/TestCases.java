cb1999e942edfce2b44be5656722669422c7d734
Samuel-Webster-178 <99287154+Samuel-Webster-178@users.noreply.github.com>
2024-04-16 19:30:59 -0400

    2-04 extra

---
+++ Unit2-04/Extra/TestCases.java
@@ -0,0 +1,73 @@
+/*
+* This program calculates the energy released when mass is converted to energy.
+*
+* @author  Samuel Webster
+* @version 1.0
+* @since   2024-03-25
+*/
+
+/**
+* This runs testcases.
+*/
+final class TestCases {
+
+    /**
+    * Prevent instantiation.
+    * Throw an exception IllegalStateException.
+    * if this is ever called
+    *
+    * @throws IllegalStateException if this is ever called
+    *
+    */
+    private TestCases() {
+        throw new IllegalStateException("Cannot be instantiated");
+    }
+
+    /**
+    * The starting main() function.
+    *
+    * @param args No args will be used
+    */
+    public static void main(final String[] args) {
+        final String popString = "popped: ";
+        final String lengthString = "length: ";
+        final String emptyString = "is empty: ";
+
+        // input
+        final SamStack fruits = new SamStack();
+        fruits.push("apple");
+        fruits.push("banana");
+        System.out.println("clearing stack:");
+        fruits.clear()
+        System.out.println("fruits: " + fruits.show());
+        System.out.println(lengthString + fruits.size());
+        System.out.println(emptyString + fruits.isEmpty());
+        System.out.println();
+
+        final SamStack colours = new SamStack();
+        colours.push("red");
+        colours.push("green");
+        colours.push("blue");
+        System.out.println(lengthString + colours.size());
+        System.out.println(popString + colours.pop());
+        System.out.println("colours: " + colours.show());
+        System.out.println(emptyString + colours.isEmpty());
+        System.out.println();
+
+        final SamStack animals = new SamStack();
+        animals.push("cat");
+        animals.push("dog");
+        animals.push("wolf");
+        animals.push("deer");
+        animals.push("rabbit");
+        System.out.println(popString + animals.pop());
+        System.out.println(popString + animals.pop());
+        System.out.println(popString + animals.pop());
+        System.out.println("peeking animals: " + animals.peek());
+        System.out.println("animals: " + animals.show());
+        System.out.println(lengthString + animals.size());
+        System.out.println();
+
+        System.out.println("\nDone.");
+    }
+}
