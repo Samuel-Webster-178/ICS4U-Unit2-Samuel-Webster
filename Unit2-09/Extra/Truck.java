9765a727b140cae2180a56130a3ca7134cdcabfc
Samuel-Webster-178 <99287154+Samuel-Webster-178@users.noreply.github.com>
2024-04-17 19:41:25 -0400

    sdgbv

---
+++ Unit2-09/Extra/Truck.java
@@ -0,0 +1,84 @@
+/*
+* This program is the standard truck class.
+*
+* @author  Samuel Webster
+* @version 1.0
+* @since   2024-03-25
+*/
+
+/**
+ * This is truck the class.
+*/
+public class Truck extends Vehicle {
+    /**
+     * Variable.
+    */
+    private String licensePlateNumber = "";
+
+    /**
+     * Variable.
+    */
+    private final int truckTires = 6;
+
+    /**
+     * Creates the base truck.
+     *
+     * @param color the base colour
+     * @param plateNumber the base license plate number
+     * @param maxSpeed the maximum speed of the vehicle
+    */
+    public Truck(
+            String color, double maxSpeed, String plateNumber
+    ) {
+        super(color, maxSpeed);
+        this.licensePlateNumber = plateNumber;
+        this.numberOfTires = truckTires;
+    }
+
+    /**
+     * Gets the license plate number of the vehicle.
+     *
+     * @return the current licensePlateNumber
+    */
+    public String getLicensePlateNumber() {
+        return this.licensePlateNumber;
+    }
+
+    /**
+     * Creates a string about the status of the vehicle.
+     *
+     * @return the string about the vehicle's status
+    */
+    public String getStatus() {
+        final String lineBreak = "\n";
+        String status = " -> Speed: " + this.getSpeed() + lineBreak;
+        status += " -> MaxSpeed: " + this.getMaxSpeed() + lineBreak;
+        status += " -> Number of tires: " + this.numberOfTires + lineBreak;
+        status += " -> Color: " + this.getColour() + lineBreak;
+        status += " -> License Plate: " + this.licensePlateNumber + lineBreak;
+
+        return status;
+    }
+
+    /**
+     * Brake using polymorphism.
+     *
+     * @param brakePower of the brake
+     * @param brakeTime of the brake
+     * @param airPressure applied to the truck
+    */
+    public void brake(
+            double brakePower, double brakeTime, double airPressure
+    ) {
+        this.setSpeed(
+                this.getSpeed() - (
+                    brakePower * brakeTime
+                ) - (
+                    airPressure * brakeTime
+                )
+        );
+        if (this.getSpeed() < 0) {
+            this.setSpeed(0);
+        }
+    }
+}
